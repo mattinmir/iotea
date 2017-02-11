fd18756ee7120f3f6be4d74c0b9b891631842d3a
root <aibrahim9449@gmail.com>
2017-02-11 15:59:10 +0000

    ch chc ch change

--- twat.py
+++ twat.py
@@ -9,26 +9,11 @@ from machine import I2C, Pin
 i2c = I2C(scl=Pin(5), sda=Pin(4))
 sensor = tsl2561.TSL2561(i2c)
 
-
-ap_if = network.WLAN(network.AP_IF)
-ap_if.active(False)
-
-sta_if = network.WLAN(network.STA_IF)
-sta_if.active(True)
-sta_if.connect('EEERover', 'exhibition')
-
-sensor.gain(16)
-sensor.integration_time(402)
-
-client = MQTTClient(machine.unique_id(), '192.168.0.10')
-client.connect()
+id = machine.unique_id()
 
 payload = ujson.dumps({'tea_id': 652510, 'name_of_tea': 'pepepepepermint tea','device_id':str(machine.unique_id()), 'broad_lux':sensor._read()[0], 'ir_lux':sensor._read()[1], 'temperature': 69})
-print(payload)
 
+#payload = ujson.dumps({'tea_id': str(id)})
 
-client.publish('esys/InternetOfThugs/tea', payload )
-
-# sudo screen /dev/ttyUSB0 115200
-# ctrl-E for paste mode
-# ctrl-D to run
+print (payload)
+print(id)
