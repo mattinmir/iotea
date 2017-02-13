51753e9646d90cf0a651276fd8931d7dfd2f05d8
Ahmed Ibrahim <aibrahim9449@gmail.com>
2017-02-13 23:22:34 +0000

    database api

--- database.py
+++ database.py
@@ -5,19 +5,20 @@ class database:
 
 	def __init__(self, _host, _port, _user, _passwd, _db):
 		self.conn = pymysql.connect(host=_host, port=_port, user=_user, passwd=_passwd, db=_db)
-		self.cursor = conn.cursor()
+		self.cursor = self.conn.cursor()
 		
 
-	def insert(self, time, tea_id, name_of_tea, type_of_tea, device_id, broad_lux, ir_lux, temperature):
-		values = "({}, {}, {}, {}, {}, {}, {}, {});".format(time, tea_id, name_of_tea, type_of_tea, device_id, broad_lux, ir_lux, temperature)
-		cursor.execute("INSERT INTO `TEA`. `teaTable` (`time`, `tea_id`, `name_of_tea`, `type_of_tea`, `device_id`, `broad_lux`, `ir_lux`, `temperature`) VALUES" + values)
+	def insert(self,  tea_id, name_of_tea, type_of_tea, device_id, broad_lux, ir_lux, temperature):
+		values = "({}, {}, {}, {}, {}, {}, {});".format(tea_id, name_of_tea, type_of_tea, device_id, broad_lux, ir_lux, temperature)
+		print(values)
+		self.cursor.execute("INSERT INTO `TEA`. `teaTable` (`tea_id`,  `name_of_tea`, `type_of_tea`, `device_id`, `broad_lux`, `ir_lux`, `temperature`) VALUES " + values)
 
 	def next_tea_id(self):
-		cursor.execute("SELECT MAX(tea_id) FROM teaTable")
-		return cursor.fetchone()[0] + 1
+		self.cursor.execute("SELECT MAX(tea_id) FROM teaTable")
+		return self.cursor.fetchone()[0] + 1
 	
 	def query(q):
-		cursor.execute(q)
-		return cursor.fetchall()
+		self.cursor.execute(q)
+		return self.cursor.fetchall()
 
 
