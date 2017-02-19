f511affd7481b3df4bd83db5cf1b7a5ceb036367
unknown <mm5213@eews506a-042.ic.ac.uk>
2017-02-19 22:23:00 +0000

    device

--- server/server.py
+++ server/server.py
@@ -2,15 +2,14 @@ import paho.mqtt.client as mqtt
 import brokerconnection 
 import brokerinfo
 
-client = mqtt.Client()
-client.on_connect = brokerconnection.on_connect
-client.on_message = brokerconnection.on_message
+# Create mqtt object
+server = mqtt.Client()
+server.on_connect = brokerconnection.on_connect
+server.on_message = brokerconnection.on_message
 
 # Connect to broker
-client.connect(brokerinfo.ip_address, brokerinfo.port, brokerinfo.keepalive)
+server.connect(brokerinfo.ip_address, brokerinfo.port, brokerinfo.keepalive)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-client.loop_forever()
+server.loop_forever()
